925a21bdb2e82e522fdfd394a07801dcc72f316f
kuangkevin <303207135@qq.com>
2016-11-16 17:41:45 +0800

    update myblogproject

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//加载路由文件
 var index = require('./routes/index');
 var users = require('./routes/users');
+var article = require('./routes/article');
 
 var app = express();
 
@@ -15,24 +17,32 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+//设置表单格式 2种格式 json和urlencoded
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+//
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+//设置路由处理模块  所有访问网站根目录的请求都由index模块处理
+//所有与用户user相关的操作请求都访问/users这个路径并且交给users路由模块处理
+//所有的路由处理全部模块化 所有访问相同资源的请求都由同一个路由模块处理
+//符合了RESTful的设计原则
 app.use('/', index);
 app.use('/users', users);
+app.use('/article',article);
 
 // catch 404 and forward to error handler
+//捕捉错误路由
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
+  //转到下一个中间件,做错误页面的渲染
   next(err);
 });
 
-// error handler
+// error handler  错误页面的渲染
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -40,7 +50,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
+  //渲染错误页面
   res.render('error');
 });
-
+//把app暴露给外界
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,12 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/reg', function(req, res, next) {
+  res.send('注册页面');
+});
+router.get('/login', function(req, res, next) {
+  res.send('登陆页面');
+});
+
 
 module.exports = router;
