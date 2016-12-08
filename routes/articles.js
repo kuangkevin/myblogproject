14782f080433206d1909ad9bea0f3605c0868e42
kuangkevin <303207135@qq.com>
2016-12-08 20:36:58 +0800

    updatemyproject

--- routes/articles.js
+++ routes/articles.js
@@ -6,38 +6,45 @@ var auto = require('../middle/autoauth');
 var router = express.Router();
 var models = require('../db/model');
 var utils = require('../utils');
-
+var multer  = require('multer');
+var markdown = require('markdown').markdown;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
 //添加文章
 router.get('/add',auto.checkLogin,function(req, res, next) {
+    //console.log(req.body);
     res.render('article/add');
 });
+
 router.post('/add',auto.checkLogin, function(req, res, next) {
     var article = req.body;
+    console.log(req.session.user._id);
     models.Article.create({
         title:article.title,
         content:article.content,
-        user:req.session.user._id,
-        createTime:{type:Date,default:new Date().now}
+        img:article.img,
+        user:req.session.user._id
     },function(err,art){
         if(err)
         {
             req.flash('error','发布失败,请稍后再试');
             res.redirect('add');
         }else{
-            console.log(art);
+            //console.log(art);
             req.flash('success','发布成功');
+            console.log(req.session.user.username);
             res.redirect('/');
         }
     })
 });
 //查看文章
-router.get('/view', function(req, res, next) {
-    res.send('查看文章');
+router.get('/detail/:id', function(req, res, next) {
+    models.Article.findById(req.params.id).populate('user').exec(function(err,articles){
+        articles.content = markdown.toHTML(articles.content);
+        res.render('article/detail',{articles:articles});
+    });
+
 });
-
-
 module.exports = router;
